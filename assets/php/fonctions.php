434b4e05b2ac5e45aaec668f49aac3395b6d9a17
Ghassen Saaf <ghassen.saaf@esprit.tn>
2020-12-08 22:38:43 +0100

    dalice last changes

---
+++ assets/php/fonctions.php
@@ -0,0 +1,29 @@
+<?php
+function getLocationInfoByIp($ip_address = ''){
+    if (empty($ip_address)) {
+        $client  = @$_SERVER['HTTP_CLIENT_IP'];
+        $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+        $server  = @$_SERVER['SERVER_ADDR'];
+        $remote  = @$_SERVER['REMOTE_ADDR'];
+        if(!empty($client) && filter_var($client, FILTER_VALIDATE_IP)){
+            $ip = $client;
+        }elseif(!empty($forward) && filter_var($forward, FILTER_VALIDATE_IP)){
+            $ip = $forward;
+        }elseif(!empty($server) && filter_var($server, FILTER_VALIDATE_IP)){
+            $ip = $server;
+        }else{
+            $ip = $remote;
+        }
+    } else {
+        $ip = $ip_address;
+    }
+
+    $ip_data = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=".$ip));
+    $result  = ['country'=>'', 'city'=>''];
+
+    if($ip_data && $ip_data['geoplugin_countryCode'] != null){
+        $result['country'] = $ip_data['geoplugin_countryCode'];
+        $result['city'] = $ip_data['geoplugin_city'];
+    }
+    return $result;
+}
